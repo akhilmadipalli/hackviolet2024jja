76b2d491ca989251008855354029c8f9891d9e25
Akhil Madipalli <akhimadi@vt.edu>
2024-02-03 14:33:48 -0500

    reset

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from lists import feminine_coded_words, masculine_coded_words
 from functions import pdf_to_str_arr
 
 
-resume = pdf_to_str_arr("resume.pdf")
+resume = pdf_to_str_arr(input("Type in your resume's relative path: "))
 for bias in feminine_coded_words:
     for word in resume:
         if word.startswith(bias):
@@ -11,4 +11,3 @@ for bias in masculine_coded_words:
     for word in resume:
         if word.startswith(bias):
             print(word)
-## time ticks by still i try
