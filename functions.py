7c6e304fc8379b82df6ac7946dd41e6408e366cd
Akhil Madipalli <akhimadi@vt.edu>
2024-02-03 16:48:10 -0500

    Updated main.py Cleaned functions.py expanded list

--- functions.py
+++ functions.py
@@ -1,4 +1,5 @@
 import pdfplumber
+from lists import feminine_coded_words, masculine_coded_words
 
 
 def pdf_to_str_arr(file_path):
@@ -15,3 +16,25 @@ def word_containing_part(part, words):
         if part in word:
             return word
     return None
+
+
+def print_fem(resume):
+    fem_list = []
+    for bias in feminine_coded_words:
+        for word in resume:
+            if word.startswith(bias):
+                fem_list.append(word)
+    fem_list = list(set(fem_list))
+    for word in fem_list:
+        print(word)
+
+
+def print_masc(resume):
+    masc_list = []
+    for bias in masculine_coded_words:
+        for word in resume:
+            if word.startswith(bias):
+                masc_list.append(word)
+    masc_list = list(set(masc_list))
+    for word in masc_list:
+        print(word)
