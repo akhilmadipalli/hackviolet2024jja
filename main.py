34a1d2c50634ab378c0445dff315a774060bbeda
Akhil Madipalli <akhimadi@vt.edu>
2024-02-03 14:15:54 -0500

    changed pdf name and made i/o in main

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 from lists import feminine_coded_words, masculine_coded_words
 from functions import pdf_to_str_arr
 
+# pdfs
+akhil = "akhil.pdf"
+john = "john.pdf"
+jin = "jin.pdf"
 
-resume = pdf_to_str_arr("resume.pdf")
+file_path = input("Type your resume's relative path: ")
+resume = pdf_to_str_arr(file_path)
 for bias in feminine_coded_words:
     for word in resume:
         if word.startswith(bias):
