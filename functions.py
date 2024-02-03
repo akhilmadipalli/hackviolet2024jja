f76313868a5fef804de813aec9673e7c75b8f2c6
Akhil Madipalli <akhimadi@vt.edu>
2024-02-03 17:14:17 -0500

    Cleaned words in pdf to txt

--- functions.py
+++ functions.py
@@ -1,4 +1,5 @@
 import pdfplumber
+import re
 from lists import feminine_coded_words, masculine_coded_words
 
 
@@ -8,7 +9,7 @@ def pdf_to_str_arr(file_path):
         for page in pdf.pages:
             text1 = page.extract_text(x_tolerance=1, y_tolerance=1)
             resume.extend(text1.lower().split())
-    return resume
+    return [clean_string(s) for s in resume]
 
 
 def word_containing_part(part, words):
@@ -38,3 +39,8 @@ def print_masc(resume):
     masc_list = list(set(masc_list))
     for word in masc_list:
         print(word)
+
+
+def clean_string(input_string):
+    cleaned_string = re.sub(r"[^a-zA-Z\-]", "", input_string)
+    return cleaned_string
